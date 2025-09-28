e51a4f5e25377be73aa51eb1eb4fbfadce0d8b9c
mrrizaldi <muhammadrafiriz23@gmail.com>
2025-09-28 21:14:45 +0700

    add route.ts on slug

--- app/[slug]/route.ts
+++ app/[slug]/route.ts
@@ -5,9 +5,9 @@ const prisma = new PrismaClient();
 
 export async function GET(
   request: NextRequest,
-  { params }: { params: { slug: string } }
+  { params }: { params: Promise<{ slug: string }> }
 ) {
-  const { slug } = params;
+  const { slug } = await params;
 
   try {
     // Query database for the slug
